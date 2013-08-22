a347af29835d75746063f9a1d580a2b370101c4b
Rahul Bhargava <rahulb@media.mit.edu>
2013-08-22 15:02:28 -0400

    switched from primary country guess to all mentioned country

--- mcgeo/db.py
+++ mcgeo/db.py
@@ -12,32 +12,79 @@ class MongoGeoStoryDatabase(MongoStoryDatabase):
         condition = {'country_code': country_code}
         initial = {'value':0}
         reduce = Code("function(doc,prev) { prev.value += 1; }") 
-        rawResults = self._db.stories.group(key, condition, initial, reduce);
-        return self._resultsToDict(rawResults,'media_id')
+        raw_results = self._db.stories.group(key, condition, initial, reduce);
+        return self._resultsToDict(raw_results,'media_id')
 
-    def storyCountByCountry(self, media_id):
-        key = ['country_code']
-        condition = {'media_id': int(media_id)}
-        initial = {'value':0}
-        reduce = Code("function(doc,prev) { prev.value += 1; }") 
-        rawResults = self._db.stories.group(key, condition, initial, reduce);
-        return self._resultsToDict(rawResults,'country_code')
+    def mentionedStoryCountByMediaSource(self, country_code):
+        mapper = Code("""
+               function () {
+                 var countryCode = '"""+country_code+"""';
+                 if(countryCode in this.country_mentions){
+                    emit(this.media_id,this.country_mentions[countryCode]);
+                 }
+               }
+               """)
+        reducer = Code("""
+                function (key, values) {
+                  var total = 0;
+                  for (var i = 0; i < values.length; i++) {
+                    total += values[i];
+                  }
+                  return total;
+                }
+                """)
+        results = self._db.stories.map_reduce(mapper,reducer, "mentionedStoryCountByMediaSource")
+        docs = []
+        for doc in results.find():
+            docs.append(doc)
+        return self._resultsToDict(docs,'_id')        
 
-    def storyCountByCountryCode(self):
+    def storyCountByCountry(self, media_id=None):
         key = ['country_code']
         condition = None
+        if media_id is not None:
+            condition = {'media_id': int(media_id)}    
         initial = {'value':0}
         reduce = Code("function(doc,prev) { prev.value += 1; }") 
-        rawResults = self._db.stories.group(key, condition, initial, reduce);
-        return self._resultsToDict(rawResults,'country_code')
+        raw_results = self._db.stories.group(key, condition, initial, reduce);
+        return self._resultsToDict(raw_results,'country_code')
+
+    def mentionedStoryCountByCountry(self,media_id=None):
+        extraJS = "";
+        if media_id is not None:
+            extraJS =  "if(this.media_id!="+media_id+") return;"
+        mapper = Code("""
+               function () {
+                 """+extraJS+"""
+                 for(var countryCode in this.country_mentions){
+                    if(countryCode.length==2){
+                        emit(countryCode,this.country_mentions[countryCode]);
+                    }
+                 }
+               }
+               """)
+        reducer = Code("""
+                function (key, values) {
+                  var total = 0;
+                  for (var i = 0; i < values.length; i++) {
+                    total += values[i];
+                  }
+                  return total;
+                }
+                """)
+        results = self._db.stories.map_reduce(mapper,reducer, "mentionedStoryCountByCountry")
+        docs = []
+        for doc in results.find():
+            docs.append(doc)
+        return self._resultsToDict(docs,'_id')
 
     # assumes key is integer!
-    def _resultsToDict(self, rawResults, id_key):
+    def _resultsToDict(self, raw_results, id_key):
         ''' 
         Helper to change a key-value set of results into a python dict
         '''
         results = {}
-        for doc in rawResults:
+        for doc in raw_results:
             key_ok = False
             try:
                 # first make sure key is an integer
--- www/server.py
+++ www/server.py
@@ -8,6 +8,8 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,parentdir) 
 from mcgeo.db import MongoGeoStoryDatabase
 
+USE_MENTIONED = True
+
 app = Flask(__name__)
 
 cache = {}  # in-memory cache, controlled by _get_from_cache and _set_in_cache helpers
@@ -29,7 +31,10 @@ else:
 
 @app.route("/")
 def index():
-    count_by_country = db.storyCountByCountryCode()
+    if USE_MENTIONED:
+        count_by_country = db.mentionedStoryCountByCountry()
+    else:
+        count_by_country = db.storyCountByCountry()
     story_count = db.storyCount()
     return render_template("global-map.html",
         story_count = story_count,
@@ -39,7 +44,10 @@ def index():
 
 @app.route("/country/<country_code>/media-coverage")
 def country_media_coverage(country_code):
-    story_count_by_media = db.storyCountByMediaSource(country_code)
+    if USE_MENTIONED:
+        story_count_by_media = db.mentionedStoryCountByMediaSource(country_code)
+    else:
+        story_count_by_media = db.storyCountByMediaSource(country_code)
     detailed_counts = []
     for media_id in story_count_by_media.keys():
         detailed_counts.append({
@@ -56,18 +64,21 @@ def country_media_coverage(country_code):
 
 @app.route("/media/<media_id>/country-coverage")
 def media_country_coverage(media_id):
-    story_count_by_country = db.storyCountByCountry(media_id)
+    if USE_MENTIONED:
+        count_by_country = db.mentionedStoryCountByCountry(media_id)
+    else:
+        count_by_country = db.storyCountByCountry(media_id)
     detailed_counts = []
-    for country_code in story_count_by_country.keys():
+    for country_code in count_by_country.keys():
         detailed_counts.append({
             'country_code': country_code,
             'country_name': _country_name(country_code),
-            'story_count': story_count_by_country[country_code]
+            'story_count': count_by_country[country_code]
         })
     return render_template("media-country-coverage.html",
         media_info = mediacloud.api.mediaSource(int(media_id)),
         coverage = sorted(detailed_counts,key=itemgetter('story_count'), reverse=True),
-        story_count = sum(story_count_by_country.values())
+        story_count = sum(count_by_country.values())
     )
 
 country_code_to_name = { 'AD': "Andorra", 'AE': "United Arab Emirates", 'AF': "Afghanistan", 'AG': "Antigua and Barbuda", 'AI': "Anguilla", 'AL': "Albania", 'AM': "Armenia", 'AO': "Angola", 'AQ': "Antarctica", 'AR': "Argentina", 'AS': "American Samoa", 'AT': "Austria", 'AU': "Australia", 'AW': "Aruba", 'AX': "Aland Islands", 'AZ': "Azerbaijan", 'BA': "Bosnia and Herzegovina", 'BB': "Barbados", 'BD': "Bangladesh", 'BE': "Belgium", 'BF': "Burkina Faso", 'BG': "Bulgaria", 'BH': "Bahrain", 'BI': "Burundi", 'BJ': "Benin", 'BL': "Saint Barthelemy", 'BM': "Bermuda", 'BN': "Brunei", 'BO': "Bolivia", 'BQ': "Bonaire, Saint Eustatius and Saba ", 'BR': "Brazil", 'BS': "Bahamas", 'BT': "Bhutan", 'BV': "Bouvet Island", 'BW': "Botswana", 'BY': "Belarus", 'BZ': "Belize", 'CA': "Canada", 'CC': "Cocos Islands", 'CD': "Democratic Republic of the Congo", 'CF': "Central African Republic", 'CG': "Republic of the Congo", 'CH': "Switzerland", 'CI': "Ivory Coast", 'CK': "Cook Islands", 'CL': "Chile", 'CM': "Cameroon", 'CN': "China", 'CO': "Colombia", 'CR': "Costa Rica", 'CU': "Cuba", 'CV': "Cape Verde", 'CW': "Curacao", 'CX': "Christmas Island", 'CY': "Cyprus", 'CZ': "Czech Republic", 'DE': "Germany", 'DJ': "Djibouti", 'DK': "Denmark", 'DM': "Dominica", 'DO': "Dominican Republic", 'DZ': "Algeria", 'EC': "Ecuador", 'EE': "Estonia", 'EG': "Egypt", 'EH': "Western Sahara", 'ER': "Eritrea", 'ES': "Spain", 'ET': "Ethiopia", 'FI': "Finland", 'FJ': "Fiji", 'FK': "Falkland Islands", 'FM': "Micronesia", 'FO': "Faroe Islands", 'FR': "France", 'GA': "Gabon", 'GB': "United Kingdom", 'GD': "Grenada", 'GE': "Georgia", 'GF': "French Guiana", 'GG': "Guernsey", 'GH': "Ghana", 'GI': "Gibraltar", 'GL': "Greenland", 'GM': "Gambia", 'GN': "Guinea", 'GP': "Guadeloupe", 'GQ': "Equatorial Guinea", 'GR': "Greece", 'GS': "South Georgia and the South Sandwich Islands", 'GT': "Guatemala", 'GU': "Guam", 'GW': "Guinea-Bissau", 'GY': "Guyana", 'HK': "Hong Kong", 'HM': "Heard Island and McDonald Islands", 'HN': "Honduras", 'HR': "Croatia", 'HT': "Haiti", 'HU': "Hungary", 'ID': "Indonesia", 'IE': "Ireland", 'IL': "Israel", 'IM': "Isle of Man", 'IN': "India", 'IO': "British Indian Ocean Territory", 'IQ': "Iraq", 'IR': "Iran", 'IS': "Iceland", 'IT': "Italy", 'JE': "Jersey", 'JM': "Jamaica", 'JO': "Jordan", 'JP': "Japan", 'KE': "Kenya", 'KG': "Kyrgyzstan", 'KH': "Cambodia", 'KI': "Kiribati", 'KM': "Comoros", 'KN': "Saint Kitts and Nevis", 'KP': "North Korea", 'KR': "South Korea", 'XK': "Kosovo", 'KW': "Kuwait", 'KY': "Cayman Islands", 'KZ': "Kazakhstan", 'LA': "Laos", 'LB': "Lebanon", 'LC': "Saint Lucia", 'LI': "Liechtenstein", 'LK': "Sri Lanka", 'LR': "Liberia", 'LS': "Lesotho", 'LT': "Lithuania", 'LU': "Luxembourg", 'LV': "Latvia", 'LY': "Libya", 'MA': "Morocco", 'MC': "Monaco", 'MD': "Moldova", 'ME': "Montenegro", 'MF': "Saint Martin", 'MG': "Madagascar", 'MH': "Marshall Islands", 'MK': "Macedonia", 'ML': "Mali", 'MM': "Myanmar", 'MN': "Mongolia", 'MO': "Macao", 'MP': "Northern Mariana Islands", 'MQ': "Martinique", 'MR': "Mauritania", 'MS': "Montserrat", 'MT': "Malta", 'MU': "Mauritius", 'MV': "Maldives", 'MW': "Malawi", 'MX': "Mexico", 'MY': "Malaysia", 'MZ': "Mozambique", 'NA': "Namibia", 'NC': "New Caledonia", 'NE': "Niger", 'NF': "Norfolk Island", 'NG': "Nigeria", 'NI': "Nicaragua", 'NL': "Netherlands", 'NO': "Norway", 'NP': "Nepal", 'NR': "Nauru", 'NU': "Niue", 'NZ': "New Zealand", 'OM': "Oman", 'PA': "Panama", 'PE': "Peru", 'PF': "French Polynesia", 'PG': "Papua New Guinea", 'PH': "Philippines", 'PK': "Pakistan", 'PL': "Poland", 'PM': "Saint Pierre and Miquelon", 'PN': "Pitcairn", 'PR': "Puerto Rico", 'PS': "Palestinian Territory", 'PT': "Portugal", 'PW': "Palau", 'PY': "Paraguay", 'QA': "Qatar", 'RE': "Reunion", 'RO': "Romania", 'RS': "Serbia", 'RU': "Russia", 'RW': "Rwanda", 'SA': "Saudi Arabia", 'SB': "Solomon Islands", 'SC': "Seychelles", 'SD': "Sudan", 'SS': "South Sudan", 'SE': "Sweden", 'SG': "Singapore", 'SH': "Saint Helena", 'SI': "Slovenia", 'SJ': "Svalbard and Jan Mayen", 'SK': "Slovakia", 'SL': "Sierra Leone", 'SM': "San Marino", 'SN': "Senegal", 'SO': "Somalia", 'SR': "Suriname", 'ST': "Sao Tome and Principe", 'SV': "El Salvador", 'SX': "Sint Maarten", 'SY': "Syria", 'SZ': "Swaziland", 'TC': "Turks and Caicos Islands", 'TD': "Chad", 'TF': "French Southern Territories", 'TG': "Togo", 'TH': "Thailand", 'TJ': "Tajikistan", 'TK': "Tokelau", 'TL': "East Timor", 'TM': "Turkmenistan", 'TN': "Tunisia", 'TO': "Tonga", 'TR': "Turkey", 'TT': "Trinidad and Tobago", 'TV': "Tuvalu", 'TW': "Taiwan", 'TZ': "Tanzania", 'UA': "Ukraine", 'UG': "Uganda", 'UM': "United States Minor Outlying Islands", 'US': "United States", 'UY': "Uruguay", 'UZ': "Uzbekistan", 'VA': "Vatican", 'VC': "Saint Vincent and the Grenadines", 'VE': "Venezuela", 'VG': "British Virgin Islands", 'VI': "U.S. Virgin Islands", 'VN': "Vietnam", 'VU': "Vanuatu", 'WF': "Wallis and Futuna", 'WS': "Samoa", 'YE': "Yemen", 'YT': "Mayotte", 'ZA': "South Africa", 'ZM': "Zambia", 'ZW': "Zimbabwe", 'CS': "Serbia and Montenegro", 'AN': "Netherlands Antilles" }
